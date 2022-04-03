e15dc00cb17ced0f96563a6c061f8ed3ab3e6d76
onur koc <onurkottz@gmail.com>
2022-04-03 14:11:47 +0300

    localStorage, icon and Readme

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice/todoSlice";
+import { throttle } from "lodash";
+import { saveState, loadState } from "./localStorage";
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
     todo: todoReducer,
   },
+  preloadedState,
 });
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
